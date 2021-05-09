d9773b99f9c44e9b859b51319460c69ebc87a748
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-05-09 00:36:30 -0500

    welcome route is working

--- server.js
+++ server.js
@@ -1,54 +1,25 @@
 let express         = require('express'),
     session         = require('express-session'),
-    cookieParser    = require('cookie-parser'),
+
     exphbs          = require('express-handlebars'),
     bodyParser      = require('body-parser'),
     path            = require('path'),
-    env             = require('dotenv').config(),
     app = express();
 //Configure Server: Set Port
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cookieParser());
-
-app.use(session({
-    key: 'user_id',
-    secret: 'MyRandKey',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
-pp.use((req, res, next) => {
-    if (req.cookies.user_id && !req.session.user) {
-        res.clearCookie('user_id');        
-    }
-    next();
-});
-
+var port = 4000;
 app.set('port', process.env.port || port); // set express to use this port
 app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
-const db = require("./app/models");//include models
-db.sequelize.sync().then(function() {
-    console.log('Nice! Database looks fine');
-}).catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update!");
-});
-
-//Include route
-require("./app/routes/vehicle.routes")(app);
-require("./app/routes/user.routes")(app);
-
 //welcome route
 app.get("/", (req, res) => {
-    res.redirect('/vehicle');
+    res.json({ message: "Welcome to auto tracker" });
 });
 
 app.listen(PORT, () => {
