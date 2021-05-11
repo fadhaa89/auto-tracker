12490e1854e94410447e51575300dd0e13cd0f4b
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-05-10 20:14:41 -0500

    renderded data to the vehicle dashboard page

--- controllers/vehicle.controller.js
+++ controllers/vehicle.controller.js
@@ -2,3 +2,30 @@ const db = require("../models");
 const User = db.users;
 const Vehicle = db.vehicles;
 const Op = db.Sequelize.Op;
+exports.all = (req, res) => {    
+    let user = req.session.user;
+    let user_id = user.id;
+    Vehicle.findAll({
+        where: {
+            user_id:user_id
+        },
+        raw: true,
+    })
+    .then(data =>{
+        res.render('index',{title:'My Vehicle',results:data});
+    }).catch(err =>{
+        res.status(500).send({
+            message: "Something went wrong"
+        })
+    });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    Vehicle.destroy({
+        where: {id}
+       }).then(() => {
+        res.redirect('/vehicle')
+        //res.render('index',{title:'My Vehicle',results:data});
+    });
+};
