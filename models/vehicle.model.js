1e5a5d4dd902d7f852f589bbca7b8db2b94bcfc7
minamahmood83 <minamahmood83@gmail.com>
2021-05-13 20:35:26 -0700

    vehicle info has done

--- models/vehicle.model.js
+++ models/vehicle.model.js
@@ -49,5 +49,7 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.INTEGER,
             allowNull: false,
         },
+        timestamps: false,
+        underscored: true,
     });
 };
