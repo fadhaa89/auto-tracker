3e387427b46a312aa7794e4b744a3e77087d84e7
minamahmood83 <minamahmood83@gmail.com>
2021-05-13 20:27:43 -0700

    create vehicle info insurance_expire

--- models/vehicle.model.js
+++ models/vehicle.model.js
@@ -33,5 +33,9 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING,
             allowNull: false,
         },
+        insurance_expire: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
     });
 };
