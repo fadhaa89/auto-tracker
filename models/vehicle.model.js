e02ac3e8c5302e2477717f3992d17880aeda08b6
minamahmood83 <minamahmood83@gmail.com>
2021-05-14 20:13:21 -0700

    fix package.json

--- models/vehicle.model.js
+++ models/vehicle.model.js
@@ -1,56 +1,56 @@
 module.exports = (sequelize, Sequelize) => {
-    const Vehicle = sequelize.define("vehicles", {
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER,
-        },
-        user_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        year: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        model: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        color: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        vin: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        license_plate: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        toll_tag_number: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        insurance_expire: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        purchase_mileage: {
-            type: Sequelize.INTEGER,
-            allowNull: true,
-        },
-        oil_change_mileage: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        tire_change_mileage: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        timestamps: false,
-        underscored: true,
-    });
+    const Vehicle = sequelize.define(
+        "vehicles", {
+            id: {
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            year: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            model: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            color: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            vin: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            license_plate: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            toll_tag_number: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            insurance_expire: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            purchase_mileage: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            },
+            oil_change_mileage: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            tire_change_mileage: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+        }, { timestamps: false, underscored: true }
+    );
     return Vehicle;
 };
--- server.js
+++ server.js
@@ -1,58 +1,61 @@
-let express         = require('express'),
-    session         = require('express-session'),
-    cookieParser    = require('cookie-parser'),
-    exphbs          = require('express-handlebars'),
-    bodyParser      = require('body-parser'),
-    path            = require('path'),
-    env             = require('dotenv').config(),
+let express = require("express"),
+    session = require("express-session"),
+    cookieParser = require("cookie-parser"),
+    exphbs = require("express-handlebars"),
+    bodyParser = require("body-parser"),
+    path = require("path"),
+    env = require("dotenv").config(),
     app = express();
-
 
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 
-app.use(session({
-    key: 'user_id',
-    secret: 'MyRandKey',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
+app.use(
+    session({
+        key: "user_id",
+        secret: "MyRandKey",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 600000,
+        },
+    })
+);
 
 app.use((req, res, next) => {
     if (req.cookies.user_id && !req.session.user) {
-        res.clearCookie('user_id');        
+        res.clearCookie("user_id");
     }
     next();
 });
 
-app.set('port', process.env.port || port); // set express to use this port
-app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(express.static(__dirname + '/public'));
+app.set("port", PORT); // set express to use this port
+app.set("views", __dirname + "/views"); // set express to look in this folder to render our view
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
 
-const db = require("./app/models");//include models
-db.sequelize.sync().then(function() {
-    console.log('Nice! Database looks fine');
-}).catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update!");
-});
-
+const db = require("./models"); //include models
+db.sequelize
+    .sync()
+    .then(function() {
+        console.log("Nice! Database looks fine");
+    })
+    .catch(function(err) {
+        console.log(err, "Something went wrong with the Database Update!");
+    });
 
 //Include route
-require("./app/routes/vehicle.routes")(app);
-require("./app/routes/user.routes")(app);
+require("./routes/vehicle.routes")(app);
+require("./routes/user.routes")(app);
 
 //welcome route
 app.get("/", (req, res) => {
-    res.redirect('/vehicle');
+    res.redirect("/vehicle");
 });
 
 app.listen(PORT, () => {
