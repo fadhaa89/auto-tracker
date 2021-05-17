21450943dbcef3eaa3702a7d3f72b81051a44fa4
minamahmood83 <minamahmood83@gmail.com>
2021-05-16 19:47:24 -0700

    update index.js

--- App/models/index.js
+++ App/models/index.js
@@ -1,22 +1,18 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
-var sequelize;
-if (process.env.NODE_ENV !== "production") {
-    sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-        host: dbConfig.HOST,
-        dialect: dbConfig.dialect,
-        operatorsAliases: false,
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
 
-        pool: {
-            max: dbConfig.pool.max,
-            min: dbConfig.pool.min,
-            acquire: dbConfig.pool.acquire,
-            idle: dbConfig.pool.idle,
-        },
-    });
-} else {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-}
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle,
+    },
+});
+
 const db = {};
 
 db.Sequelize = Sequelize;
